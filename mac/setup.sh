afdecbeed9d69b3d2d6926eb2c5aa8b01c3efb20
Anders Finserås Graneng <graneng97@gmail.com>
2021-07-10 16:00:16 +0200

    add mac setup

---
+++ mac/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo 'Set Screenshot folder to ~/Downloads'
+defaults write com.apple.screencapture location ~/Downloads
+killall SystemUIServer
+
+echo "Finder: show all filename extensions"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+echo "cmd+shift+left and cmd+shift+right to move apps to left and right of the screen"
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "Move Window to Left Side of Screen" "@$\\U2190"
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "Move Window to Right Side of Screen" "@$\\U2192"
+
+# Check if Homebrew is installed
+command -v brew >/dev/null 2>&1 || { \
+	echo >&2 "Installing Homebrew Now"; \
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; \
+}
+
+brew_cask_check_if_installed () {
+	if brew ls --cask --versions $1 > /dev/null; then
+		echo "$1 allready installed"
+	else
+		echo "$1 is not installed with brew cask"
+		read -p "Install it? [yN] " -n 1 -r
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			echo
+			echo "Installing $1"
+			brew install --cask $1
+		fi
+	fi
+}
+
+
+brew install git
+brew install --cask iTerm2
+
+
