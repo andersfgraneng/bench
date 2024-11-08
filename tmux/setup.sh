9735ba552811fa6f5d553319f736ebf1284a8f3a
Anders Finserås Graneng <graneng97@gmail.com>
2024-11-08 22:47:14 +0100

    add script for installing .tmux

---
+++ tmux/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+if [[ ! -d "$HOME/.tmux" ]]; then
+  git clone https://github.com/gpakosz/.tmux.git "$HOME/.tmux"
+  ln -s -f "$HOME/.tmux/.tmux.conf"
+  cp "$HOME/.tmux/.tmux.conf.local" "$HOME"
+fi
