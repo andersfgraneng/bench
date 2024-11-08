a71f4f87e8c502b642d12d33ccc5dc0d271ed42c
Anders Finserås Graneng <graneng97@gmail.com>
2024-11-08 11:38:37 +0100

    add support for rust

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,40 +1,43 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls", "angularls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls", "angularls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities,
+      })
 
-			local util = require("lspconfig.util")
-			lspconfig.angularls.setup({
-				capabilities = capabilities,
-				filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx", "htmlangular"},
-				root_dir = util.root_pattern("angular.json", "project.json"),
-			})
+      local util = require("lspconfig.util")
+      lspconfig.angularls.setup({
+        capabilities = capabilities,
+        filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx", "htmlangular" },
+        root_dir = util.root_pattern("angular.json", "project.json"),
+      })
 
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-		end,
-	},
+    end,
+  },
 }
