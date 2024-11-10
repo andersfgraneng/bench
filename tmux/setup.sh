715449d95e2a82d57e9d3cff5f5f72d4fa145446
Anders Finserås Graneng <graneng97@gmail.com>
2024-11-10 22:55:58 +0100

    update tmux config and setup

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -3,8 +3,3 @@
 if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
-
-if [[ ! -d "$HOME/.config/tmux/plugins/catpuccin" ]]; then
-  mkdir -p ~/.config/tmux/plugins/catppuccin
-  git clone -b v2.1.0 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
-fi
