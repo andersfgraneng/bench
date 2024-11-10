63755c889936b5a96c8daa331331ac226b2546a6
Anders Finserås Graneng <graneng97@gmail.com>
2024-11-10 12:23:57 +0100

    add cattpuccin tmux theme

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -5,3 +5,8 @@ if [[ ! -d "$HOME/.tmux" ]]; then
   ln -s -f "$HOME/.tmux/.tmux.conf"
   cp "$HOME/.tmux/.tmux.conf.local" "$HOME"
 fi
+
+if [[ ! -d "$HOME/.config/tmux/plugins/catpuccin" ]]; then
+  mkdir -p ~/.config/tmux/plugins/catppuccin
+  git clone -b v2.1.0 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+fi
