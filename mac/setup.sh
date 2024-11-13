0891a1d9f3f6b16ab2328127b0bca0dfdccc1ce1
Anders Finserås Graneng <graneng97@gmail.com>
2024-11-13 09:36:56 +0100

    add aerospace tiling manager

--- mac/setup.sh
+++ mac/setup.sh
@@ -36,9 +36,9 @@ brew_cask_check_if_installed () {
 
 
 brew install git
-brew install --cask iTerm2
 brew install tmux
-brew install tmuxinator
 brew install fd
 brew install neovim
 brew install fzf
+brew install --cask iTerm2
+brew install --cask nikitabobko/tap/aerospace
--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 # Run setup for all modules
 
+./mac/setup.sh
 ./nvim/setup.sh
 ./tmux/setup.sh
 ./zsh/setup.sh
