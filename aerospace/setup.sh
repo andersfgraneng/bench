c0abc82afbbf97f1ad08d25522687404654f60df
Anders Finserås Graneng <graneng97@gmail.com>
2024-11-13 11:58:31 +0100

    add aerospace setup

---
+++ aerospace/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+echo "\nSetup aerospace\n----------"
+
+if [[ ! -f "$HOME/.tmux.conf" ]]; then
+  ln -s "${0:a:h}/.aerospace.toml" "$HOME/.aerospace.toml"
+  echo "Creating symlink for ~/.aerospace.toml"
+else
+  echo "~/.aerospace.toml already exists."
+fi
