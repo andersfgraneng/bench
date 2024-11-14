6cbbce51880dfeeb0943000dfeb71c60e3275864
Anders Finserås Graneng <graneng97@gmail.com>
2024-11-14 22:39:03 +0100

    display hidden files in neo-tree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -7,17 +7,18 @@ return {
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        show_hidden_count = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-      },
-    },
-  },
   config = function()
     vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+      },
+    })
   end,
 }
