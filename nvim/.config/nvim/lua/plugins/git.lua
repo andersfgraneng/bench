5096077d38afd19ce16921d577b08e4e74ce891f
Anders Finserås Graneng <graneng97@gmail.com>
2024-12-02 16:01:48 +0100

    nvim: update git

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,8 +1,18 @@
 return {
-  {
-    "f-person/git-blame.nvim",
+	{
+    "f-person/git-blame.nvim"
   },
-  {
-    "sindrets/diffview.nvim",
-  },
+	{
+		"sindrets/diffview.nvim",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+	},
 }
