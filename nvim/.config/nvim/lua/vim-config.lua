9baa1e5c5ad4feadcbc40cb2c67a1528b96fa018
Anders Finserås Graneng <graneng97@gmail.com>
2025-01-31 12:30:59 +0100

    configure jdtls

--- nvim/.config/nvim/lua/vim-config.lua
+++ nvim/.config/nvim/lua/vim-config.lua
@@ -9,3 +9,12 @@ vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+vim.keymap.set("n", "<leader>df", "<Cmd>lua require'jdtls'.test_class()<CR>") -- nnoremap  <Cmd>lua require'jdtls'.test_class()<CR>
+vim.keymap.set("n", "<leader>dn", "<Cmd>lua require'jdtls'.test_nearest_method()<CR>") -- nnoremap  <Cmd>lua require'jdtls'.test_nearest_method()<CR>
+vim.keymap.set("n", "<A-o>", "<Cmd>lua require'jdtls'.organize_imports()<CR>") -- nnoremap <A-o> <Cmd>lua require'jdtls'.organize_imports()<CR>
+vim.keymap.set("n", "crv", "<Cmd>lua require('jdtls').extract_variable()<CR>") -- nnoremap crv <Cmd>lua require('jdtls').extract_variable()<CR>
+vim.keymap.set("v", "crv", "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>") -- vnoremap crv <Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>
+vim.keymap.set("n", "crc", "<Cmd>lua require('jdtls').extract_constant()<CR>") -- nnoremap crc <Cmd>lua require('jdtls').extract_constant()<CR>
+vim.keymap.set("v", "crc", "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>") -- vnoremap crc <Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>
+vim.keymap.set("v", "crm", "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>") -- vnoremap crm <Esc><Cmd>lua require('jdtls').extract_method(true)<CR>
