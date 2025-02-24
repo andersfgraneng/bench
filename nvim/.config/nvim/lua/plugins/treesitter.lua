2b02b06cd870246714adcda45862325b0e1f65f1
Anders Finserås Graneng <graneng97@gmail.com>
2025-02-24 07:42:48 +0100

    nvim: add react

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,34 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-        ensure_installed = {
-          "lua",
-          "vim",
-          "vimdoc",
-          "javascript",
-          "typescript",
-          "html",
-          "java",
-          "rust",
-          "angular",
-          "json",
-          "yaml",
-          "terraform",
-          "hcl",
-          "go",
-          "dockerfile"
-        },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
-  { "dlvandenberg/tree-sitter-angular" },
+			configs.setup({
+				ensure_installed = {
+					"lua",
+					"vim",
+					"vimdoc",
+					"html",
+					"javascript",
+					"typescript",
+					"tsx",
+					"angular",
+					"java",
+					"rust",
+					"json",
+					"yaml",
+					"terraform",
+					"hcl",
+					"go",
+					"dockerfile",
+				},
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{ "dlvandenberg/tree-sitter-angular" },
 }
