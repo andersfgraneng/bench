dc89536895cf2d7a987496f1579fec3d0b0badd1
Anders Finserås Graneng <graneng97@gmail.com>
2026-03-10 22:17:29 +0100

    nvim: add pyrefly lsp for python

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,32 +1,33 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					-- general programming languages/tools
-					"jdtls", -- java
-					"rust_analyzer", -- rust
-					"lua_ls", -- lua
-					"taplo", -- toml
-					-- build/infrastructure tools
-					"dockerls", -- docker
-					"gh_actions_ls", -- github actions
-					-- frontend tools/languages/frameworks
-					"ts_ls", -- typescript
-					"astro", -- astro
-					"angularls", -- angular
-					"tailwindcss", -- tailwind
-					"html", -- html
-				},
-			})
-		end,
-	},
-	"neovim/nvim-lspconfig",
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    -- general programming languages/tools
+                    "jdtls", -- java
+                    "rust_analyzer", -- rust
+                    "lua_ls", -- lua
+                    "taplo", -- toml
+                    "pyrefly", -- python
+                    -- build/infrastructure tools
+                    "dockerls", -- docker
+                    "gh_actions_ls", -- github actions
+                    -- frontend tools/languages/frameworks
+                    "ts_ls", -- typescript
+                    "astro", -- astro
+                    "angularls", -- angular
+                    "tailwindcss", -- tailwind
+                    "html", -- html
+                },
+            })
+        end,
+    },
+    "neovim/nvim-lspconfig",
 }
