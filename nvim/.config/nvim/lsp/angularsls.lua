6757f4f82cea7f8414a56bdc03042a69dc2859ca
Anders Finserås Graneng <graneng97@gmail.com>
2026-04-08 14:31:52 +0200

    nvim: remove lsp files

--- nvim/.config/nvim/lsp/angularsls.lua
+++
@@ -1,5 +0,0 @@
-local util = require("lspconfig.util")
-return {
-	filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx", "htmlangular" },
-	root_dir = util.root_pattern("angular.json", "project.json"),
-}
