324b718c96fedf140586193fe81c10b20251a807
Anders Finserås Graneng <graneng97@gmail.com>
2026-04-08 10:58:09 +0200

    nvim: use replace nvim-cmp with blink.cmp

--- nvim/.config/nvim/lsp/angularsls.lua
+++ nvim/.config/nvim/lsp/angularsls.lua
@@ -1,7 +1,5 @@
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local util = require("lspconfig.util")
 return {
-	capabilities = capabilities,
 	filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx", "htmlangular" },
 	root_dir = util.root_pattern("angular.json", "project.json"),
 }
--- nvim/.config/nvim/lsp/ts_ls.lua
+++ nvim/.config/nvim/lsp/ts_ls.lua
@@ -1,10 +1,8 @@
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- frontend tools/languages/frameworks
 -- for ts remember you need to:
 -- npm install -g typescript typescript-language-server
 return {
-	capabilities = capabilities,
 	filetypes = {
 		"javascript",
 		"typescript",
