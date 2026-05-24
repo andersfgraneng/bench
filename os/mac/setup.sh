aa5651256d98f9467c1043671c6ecc6932a89ac3
Anders Finserås Graneng <graneng97@gmail.com>
2026-05-24 11:43:02 +0200

    os(mac): use brewfile

--- os/mac/setup.sh
+++ os/mac/setup.sh
@@ -23,54 +23,5 @@ command -v brew >/dev/null 2>&1 || { \
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; \
 }
 
-brew_cask_check_if_installed () {
-	if brew ls --cask --versions $1 > /dev/null; then
-		echo "$1 allready installed"
-	else
-		echo "$1 is not installed with brew cask"
-		read -p "Install it? [yN] " -n 1 -r
-		if [[ $REPLY =~ ^[Yy]$ ]]; then
-			echo
-			echo "Installing $1"
-			brew install --cask $1
-		fi
-	fi
-}
-
 brew update
-
-brew install git
-brew install tmux
-brew install neovim
-brew install starship
-brew install hurl
-brew install stow
-brew install openconnect
-brew install fd
-brew install jq
-brew install bat
-brew install tree
-brew install zip
-brew install unzip
-brew install tree-sitter
-brew install tree-sitter-cli
-brew install fzf
-brew install ripgrep
-brew install docker
-brew install docker-compose
-brew install docker-credential-helper
-brew install colima
-brew install 1password-cli
-brew install --cask 1password
-brew install --cask ghostty
-brew install --cask obsidian
-brew install --cask discord
-brew install --cask zed
-
-# install programming languages tooling
-
-brew install rust
-brew install uv
-brew install fnm
-brew tap hashicorp/tap
-brew install hashicorp/tap/terraform
+brew bundle --file="$(dirname "$0")/Brewfile"
